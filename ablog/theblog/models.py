6b0f227fbdf340b101869aa76d3b9f5a046c00a6
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-08-05 02:15:35 +0530

    snippet added to show and edit profile feature added

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -5,8 +5,11 @@ from datetime import datetime, date
 from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
@@ -14,14 +17,18 @@ class Category(models.Model):
         # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     # body = models.TextField()
-    post_date = models.DateField(auto_now_add = True)
+    post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='all')
+    snippet = models.CharField(
+        max_length=255)
+
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def __str__(self):
@@ -30,5 +37,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
+
     def total_likes(self):
         return self.likes.count()
